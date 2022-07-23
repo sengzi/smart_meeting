4bdb974827cf03f3348f6781d470a265506b9451
sengzi <90605936+sengzi@users.noreply.github.com>
2022-07-23 20:32:42 +0800

    Update urls.py

--- smart_meeting/urls.py
+++ smart_meeting/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views
 
 from django.views.static import serve
-from django.conf.urls import url
+from django.urls import re_path as url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
